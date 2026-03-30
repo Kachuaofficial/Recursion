a3d419f4257f962db854eb990c8678cebd61d243
kachuaofficial <shivammisra2021@gmail.com>
2026-03-30 18:30:40 +0530

    added recursion of reverse of a number

--- src/aRaisedToPowerB.java
+++ src/aRaisedToPowerB.java
@@ -1,6 +1,19 @@
-public class aRaisedToPowerB {
+import static java.lang.IO.print;
 
-    void main() {
+void main() {
+    int pow= 4;
+    int a = 2;
+
+    print(power(a,pow));
+
+}
+
+int power(int num, int pow) {
+    if(pow == 0) {
+        return 1;
 
     }
-}
+
+    return num * power(num, pow - 1);
+};
+
